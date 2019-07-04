d28a7216221d2e840e9fe6b5df2c2e5208a62f4d
opherv <opher@interlude.fm>
2019-07-04 11:21:28 +0300

    Placeholder

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <svg viewBox="0 0 71 32" className="App-logo">
+          <path d="M118.2,25l-8.331,0-6.91,10.924V16H95.883V47.469h6.189l4.5-6.271,4.5,6.271h9.054L110.75,35.374Zm10.975-.549A11.777,11.777,0,1,0,141,36.223,11.8,11.8,0,0,0,129.172,24.446Zm0,16.773a4.784,4.784,0,0,1-4.731-5,4.739,4.739,0,1,1,9.462,0A4.784,4.784,0,0,1,129.172,41.219ZM81.828,24.446A11.777,11.777,0,1,0,92.9,40.382H85.7c-0.741.876-1.7,1.5-3.812,1.5a4.856,4.856,0,0,1-4.807-3.438H93.445A11.778,11.778,0,0,0,81.828,24.446ZM77.082,34a5.017,5.017,0,0,1,9.491,0H77.082Z" transform="translate(-70 -16)"></path>
+        </svg>
+        <p>
+          Eko.Engineering will soon be up :)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
