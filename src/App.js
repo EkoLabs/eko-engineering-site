a44e3f3db8bbfc5980f53ce290a0d1e9dce82a3f
Opher Vishnia <opherv@gmail.com>
2019-07-07 22:58:33 +0300

    Add placeholder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
+import Placeholder from "./components/placeholder/Placeholder";
 
 function App() {
   return (
       <Router>
         <div className="App">
-          <Route path="/" exact component={Homepage} />
+          <Route path="/" exact component={Placeholder} />
+          <Route path="/dev" exact component={Homepage} />
         </div>
       </Router>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,12 +12,12 @@ function Header({ location: { hash } }){
         <header>
            <a className="logo">Eko <span>Engineering</span></a>
            <nav>
-               <a href="/#" className={hash===''?'selected':''}>Home</a>
-               <a href="/#about" className={hash==='#about'?'selected':''} >About</a>
-               <a href="/#tech" className={hash==='#tech'?'selected':''}>Tech</a>
-               <a href="/#ecosystem" className={hash==='#ecosystem'?'selected':''} >Ecosystem</a>
-               <a href="/#careers" className={hash==='#careers'?'selected':''} >Careers</a>
-               <a href="/#contact" className={hash==='#contact'?'selected':''} >Contact</a>
+               <a href="#" className={hash===''?'selected':''}>Home</a>
+               <a href="#about" className={hash==='#about'?'selected':''} >About</a>
+               <a href="#tech" className={hash==='#tech'?'selected':''}>Tech</a>
+               <a href="#ecosystem" className={hash==='#ecosystem'?'selected':''} >Ecosystem</a>
+               <a href="#careers" className={hash==='#careers'?'selected':''} >Careers</a>
+               <a href="#contact" className={hash==='#contact'?'selected':''} >Contact</a>
            </nav>
         </header>
     )
