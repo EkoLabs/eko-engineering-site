6be15383553bf2787526e697128af2aafb676d68
Yariv Fruend <yariv@helloeko.com>
2019-07-07 14:48:06 +0300

    Form fields are now required

--- src/components/contactform/ContactForm.jsx
+++ src/components/contactform/ContactForm.jsx
@@ -8,15 +8,15 @@ function ContactForm(props){
                 <h4>Questions about Joining Eko?</h4>
                 <form className="contact-form">
                     <div className="form-field">
-                        <input className="input-text" type="input-text" maxlength="100"/>
+                        <input className="input-text" type="input-text" maxlength="100" required/>
                             <label className="label" htmlFor="name">Name</label>
                     </div>
                     <div className="form-field">
-                        <input className="input-text" type="email" maxlength="100"/>
+                        <input className="input-text" type="email" maxlength="100" required/>
                         <label className="label" htmlFor="email">E-mail</label>
                     </div>
                     <div className="form-field">
-                        <textarea className="input-text" placeholder="message" maxlength="2000"></textarea>
+                        <textarea className="input-text" placeholder="message" maxlength="2000" required></textarea>
                     </div>
                     <div className="form-field">
                         <button className="ctaButton">Send</button>
