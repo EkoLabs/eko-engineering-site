746535c385dfa1cac3e376f124552201d8d46b3d
Yariv Fruend <yariv@helloeko.com>
2019-07-08 10:04:44 +0300

    Updated investor logos

--- src/components/investors/Investors.jsx
+++ src/components/investors/Investors.jsx
@@ -2,29 +2,53 @@ import React from "react";
 import "./Investors.scss";
 
 import Sequoia from "./logos/Sequoia.svg";
+import NEA from "./logos/NEA.svg";
+import Marker from "./logos/Marker.svg";
+import Innovation from "./logos/Innovation.png";
 import Sony from "./logos/Sony.svg";
-import Intel from "./logos/Intel.svg";
-import Samsung from "./logos/Samsung.svg";
 import MGM from "./logos/MGM.svg";
+import Intel from "./logos/Intel.svg";
+import Warner from "./logos/Warner.svg";
+import Samsung from "./logos/Samsung.svg";
+import Wpp from "./logos/Wpp.svg";
+import Walmart from "./logos/Walmart.svg";
 
 function Investors(props) {
     props = {
         title: "Backed by top investors",
         items: [
             {
-                imageURL: Sequoia,
+                imageURL: Sequoia
             },
             {
-                imageURL: Sony,
+                imageURL: NEA
             },
             {
-                imageURL: Intel,
+                imageURL: Marker
             },
             {
-                imageURL: Samsung,
+                imageURL: Innovation
             },
             {
-                imageURL: MGM,
+                imageURL: Sony
+            },
+            {
+                imageURL: MGM
+            },
+            {
+                imageURL: Intel
+            },
+            {
+                imageURL: Warner
+            },
+            {
+                imageURL: Samsung
+            },
+            {
+                imageURL: Wpp
+            },
+            {
+                imageURL: Walmart
             },
         ]
     }
