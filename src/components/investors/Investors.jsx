52b64b22117c4b2c2f461c089528927f49cfc7ea
Yariv Fruend <yariv@helloeko.com>
2019-07-08 19:03:33 +0300

    Investors grid && Investors responsiveness

--- src/components/investors/Investors.jsx
+++ src/components/investors/Investors.jsx
@@ -4,7 +4,7 @@ import "./Investors.scss";
 import Sequoia from "./logos/Sequoia.svg";
 import NEA from "./logos/NEA.svg";
 import Marker from "./logos/Marker.svg";
-import Innovation from "./logos/Innovation.png";
+import Innovation from "./logos/Innovation_black.png";
 import Sony from "./logos/Sony.svg";
 import MGM from "./logos/MGM.svg";
 import Intel from "./logos/Intel.svg";
@@ -17,49 +17,82 @@ function Investors(props) {
     props = {
         title: "Backed by top investors",
         items: [
-            {
-                imageURL: Sequoia
-            },
-            {
-                imageURL: NEA
-            },
-            {
-                imageURL: Marker
-            },
-            {
-                imageURL: Innovation
-            },
-            {
-                imageURL: Sony
-            },
-            {
-                imageURL: MGM
-            },
-            {
-                imageURL: Intel
-            },
-            {
-                imageURL: Warner
-            },
-            {
-                imageURL: Samsung
-            },
-            {
-                imageURL: Wpp
-            },
-            {
-                imageURL: Walmart
-            },
+            [
+                [
+                    {
+                        imageURL: Sequoia,
+                        URL: "https://www.sequoiacap.com/companies/eko/"
+                    },
+                    {
+                        imageURL: NEA,
+                        URL: "http://www.nea.com/"
+                    },
+                    {
+                        imageURL: Marker,
+                        URL: "http://www.marker-llc.com/"
+                    }
+                ],
+                [
+                    {
+                        imageURL: Innovation,
+                        URL: "http://www.innovationendeavors.com/"
+                    },
+                    {
+                        imageURL: Sony,
+                        URL: "http://www.sonypictures.com/corp/press_releases/2016/06_16/061616_interlude.html"
+                    },
+                    {
+                        imageURL: MGM,
+                        URL: "http://www.businesswire.com/news/home/20151208005564/en/Interlude-Announces-Strategic-Investment-Metro-Goldwyn-Mayer-Studios"
+                    }
+                ]
+            ],
+            [
+                [
+                    {
+                        imageURL: Intel,
+                        URL: "http://www.intelcapital.com/"
+                    },
+                    {
+                        imageURL: Warner,
+                        URL: "http://www.wmg.com/news/warner-music-group-becomes-founding-major-label-partner-interlude-music-19996"
+                    },
+                    {
+                        imageURL: Samsung,
+                        URL: "http://www.businesswire.com/news/home/20151105005316/en/Samsung-Strategic-Investment-Interlude"
+                    }
+                ],
+                [
+                    {
+                        imageURL: Wpp,
+                        URL: "http://www.wpp.com/wpp/companies/atinterlude/"
+                    },
+                    {
+                        imageURL: Walmart,
+                        URL: "https://news.walmart.com/2018/10/11/walmart-and-eko-announce-joint-venture-to-create-interactive-storytelling-for-entertainment-and-retail"
+                    }
+                ]
+            ]
         ]
     }
 
-    let items = props.items.map((item, i) => (
-        <li className="item" key={i}>
-            <img src={item.imageURL} alt="Eko investor" />
-        </li>
+    let items = props.items.map((row, i) => (
+        <ul className="row">
+            {row.map(innerRow => (
+                <ul className="inner-row">
+                    {innerRow.map(item => (
+                        <a target="_blank" href={item.URL}>
+                            <li className="item" key={i}>
+                                <img src={item.imageURL} alt="Eko investor" />
+                            </li>
+                        </a>
+                    ))}
+                </ul>
+            ))}
+        </ul>
     ));
 
-    return(
+    return (
         <section>
             <div className="content investors">
                 <h4>{props.title}</h4>
