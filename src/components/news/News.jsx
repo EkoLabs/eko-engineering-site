b3abe388f6acbd2bcc5bb2ee135439662fd29f56
opherv <opher@interlude.fm>
2019-07-08 16:03:31 +0300

    Add links to news

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -15,25 +15,25 @@ function News(){
                 title: "The Story with a Thousand Endings",
                 subtitle: "The New Yorker",
                 imageURL: NewYorker,
-                URL: ""
-            },
-            {
-                title: "Walmart Toy Lab Is The Best Thing Walmart Has Done All Year",
-                subtitle: "Forbes",
-                imageURL: Forbes,
-                URL: ""
+                URL: "https://www.newyorker.com/magazine/2017/01/30/alternate-endings"
             },
             {
                 title: "Bob Dylan Goes Interactive in ‘Like a Rolling Stone’ Clip",
                 subtitle: "ROLLINGSTONE",
                 imageURL: RollingStone,
-                URL: ""
+                URL: "https://www.rollingstone.com/music/music-news/bob-dylan-goes-interactive-in-like-a-rolling-stone-clip-203780/"
             },
             {
                 title: "Proper choose-your-own-adventure TV series are here, but don’t call them that",
                 subtitle: "Polygon",
                 imageURL: Polygon,
-                URL: ""
+                URL: "https://www.polygon.com/2017/11/14/16637752/choose-your-own-adventure-tv-sony-eko-hbo-twitch"
+            },
+            {
+                title: "Walmart Toy Lab Is The Best Thing Walmart Has Done All Year",
+                subtitle: "Forbes",
+                imageURL: Forbes,
+                URL: "https://www.forbes.com/sites/christopherwalton/2018/11/30/walmart-toy-lab-is-the-best-thing-walmart-has-done-all-year/#118db1e743e4"
             },
         ]
     };
