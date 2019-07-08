1842270a30c0eaede86a7c6fe4c174a73f215f84
Yariv Fruend <yariv@helloeko.com>
2019-07-08 15:29:38 +0300

    Mobile Menu | https://app.asana.com/0/0/1130120786684516/f

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,4 +1,8 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
+import { withRouter } from 'react-router';
+import crossIcon from '../header/mobile-menu/cross.svg';
+import burgerIcon from '../header/mobile-menu/burger.svg';
+import { slide as Menu } from 'react-burger-menu';
 import Header from "../header/Header";
 import Splash from "../splash/Splash";
 import Tech from "../tech/Tech";
@@ -13,6 +17,14 @@ import Office from "../office/Office";
 function Homepage(){
     return (
         <Fragment>
+            <Menu bodyClassName={"menu-open"} right customBurgerIcon={<img src={burgerIcon} />} customCrossIcon={<img src={crossIcon} />} >
+                <a id="home" className="menu-item" href="#">Home</a>
+                <a id="about" className="menu-item" href="#">About</a>
+                <a id="tech" className="menu-item" href="#">Tech</a>
+                <a id="ecosystem" className="menu-item" href="#">Ecosystem</a>
+                <a id="careers" className="menu-item" href="#">Careers</a>
+                <a id="contact" className="menu-item" href="#">Contact</a>
+            </Menu>
             <Header/>
             <Splash/>
             <Tech/>
