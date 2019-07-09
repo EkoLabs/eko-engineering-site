00de4216dbb7abff3d7fe539830ec1cfd7a4eab7
opherv <opher@interlude.fm>
2019-07-09 14:24:21 +0300

    About us: add icons

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./About.scss";
-import placeholderIcon from './icon_devs_placeholder.png';
+import createIcon from './icon_bulb.svg';
+import designIcon from './icon_monitor_paint.svg';
+import buildIcon from './icon_tools.svg';
+import developersIcon from './icon_developers.svg';
 import ScrollableAnchor from "react-scrollable-anchor";
 
 function About(props) {
@@ -8,22 +11,22 @@ function About(props) {
         title: 'About us',
         items: [
             {
-                icon: placeholderIcon,
+                icon: createIcon,
                 title: 'We Create',
                 description: 'We come from diverse engineering backgrounds: from web and design to hardware and embedded systems. We’re also passionate about art, entertainment, technology and everything in between. It’s our unique mix of talents and passions which puts us at the forefront of interactive video.'
             },
             {
-                icon: placeholderIcon,
+                icon: designIcon,
                 title: 'We Design Experiences',
                 description: 'We’re excited about making experiences that are not only clear but engaging and fun. Get ready for a world full of motion and animations over a wide range of clients such as webapps, mobile, Xbox, Steam, VR and smart TVs apps.'
             },
             {
-                icon: placeholderIcon,
+                icon: buildIcon,
                 title: 'We Build Cool Stuff',
                 description: 'Our core technology enables generating and manipulating media streams in real time. On top of that, our scalable cloud-based system serves our creators and millions of viewers. We’re constantly solving complex problems by combining proven solutions with bold ideas. '
             },
             {
-                icon: placeholderIcon,
+                icon: developersIcon,
                 title: 'We ARE Developers',
                 description: 'As a developer at Eko, you’ll be creating the tech and infrastructure for a new era of digital storytelling. Choose your own hardware and software as you lead projects, take an active part in the creative process, enjoy a flat organizational structure and push interactive video to the edge, or over it!'
             },
@@ -33,7 +36,7 @@ function About(props) {
     let items = props.items.map((item, i) => (
         <div className="item-container">
             <div className="item" key={i}>
-                <img src={placeholderIcon} alt="About us icon" />
+                <img src={item.icon} alt={item.title} />
                 <div className="about-title">{item.title}</div>
                 <div className="about-description">{item.description}</div>
             </div>
