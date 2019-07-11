c4c60bccdfa0f9086bdf13fbddbfde26cec5b3b5
Opher Vishnia <opherv@gmail.com>
2019-07-11 12:12:28 +0300

    Fix mobile menu

--- src/components/mobilemenu/MobileMenu.jsx
+++ src/components/mobilemenu/MobileMenu.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { slide as Menu } from 'react-burger-menu';
 
 import burgerIcon from "./burger.svg";
 import crossIcon from "./cross.svg";
 
 function MobileMenu(){
+    const [isOpen, setOpen] = useState(false);
+
+    function itemClicked(){
+        setOpen(false);
+    }
+
     return (
-        <Menu bodyClassName={"menu-open"} right customBurgerIcon={<img src={burgerIcon} alt="Open Menu" />} customCrossIcon={<img src={crossIcon} alt="Close Menu"/>} >
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="#about">About</a>
-            <a id="tech" className="menu-item" href="#tech">Tech</a>
-            <a id="ecosystem" className="menu-item" href="#ecosystem">Ecosystem</a>
-            <a id="careers" className="menu-item" href="#careers">Careers</a>
-            <a id="contact" className="menu-item" href="#contact">Contact</a>
+        <Menu bodyClassName={"menu-open"}
+              isOpen = {isOpen}
+              onStateChange={ state=>setOpen(state.isOpen)}
+              right
+              customBurgerIcon={<img src={burgerIcon} alt="Open Menu" />}
+              customCrossIcon={<img src={crossIcon}
+              alt="Close Menu"/>} >
+            <a id="home" onClick={itemClicked} className="menu-item" href="/">Home</a>
+            <a id="about" onClick={itemClicked} className="menu-item" href="#about">About</a>
+            <a id="tech" onClick={itemClicked} className="menu-item" href="#tech">Tech</a>
+            <a id="ecosystem" onClick={itemClicked} className="menu-item" href="#ecosystem">Ecosystem</a>
+            <a id="careers" onClick={itemClicked} className="menu-item" href="#careers">Careers</a>
+            <a id="contact" onClick={itemClicked} className="menu-item" href="#contact">Contact</a>
         </Menu>
     );
 }
