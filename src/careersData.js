142ed5a63961f7d9eac4a8b0d724a626734ada63
Yariv Fruend <yariv@helloeko.com>
2020-01-19 12:01:30 +0200

    Revert "Revert "Update fullstack position text""
    This reverts commit 8af042d8289448881fc99a63fd166936d5d181cb.

--- src/careersData.js
+++ src/careersData.js
@@ -101,47 +101,78 @@ Want to learn more?
         },
         {
             title: 'Junior Fullstack Developer',
+            introText: `
+eko is a media technology company pioneering a new medium in which viewers shape live action video stories as they unfold. Our award-winning shows are distributed through [eko.com](https://eko.com), affiliate partners, and social networks; and are viewable on desktop, mobile, connected TVs, and VR.
+
+We’re looking for a developer passionate about new technology and willing to push the envelope of what can be done on the web. Our platform is mainly web-based, so you’ll be working heavily with JavaScript, as well as other web libraries and tools (React, Redux, Angular, Node.js, and hapi to name a few). Our ultimate goal, though, is to always be at the forefront of video entertainment technology, so we’re not tied to any one tool and we’re always experimenting.
+
+At eko, you’ll: 
+* Contribute to every part of our stack- from working with SQL databases to building microservices to crafting complex UI
+* Have full ownership and responsibility over your feature work
+* Collaborate with almost every other department in the company
+* Become an expert in JavaScript rendering, I/O handling, and data processing
+* Navigate the challenges of supporting multiple environments and devices like computers, mobile phones, tablets, smart TVs, and gaming consoles
+* Help design and define what choice-driven entertainment really is, and what makes it tick
+
+*(and the dogs, snacks, happy hour jam sessions, and general speak-your-mind creative environment will keep things interesting when you’re not JavaScripting)*
+
+If you are all about creating mind-blowing experiences on the web, consider yourself invited!
+
+Want to learn more?
+            `,
             text: `
 ##### Key Requirements
+* Programming skills - deep understanding of at least one programming language. Easily able to identify and effectively communicate strengths/weakness in code. Experience with JavaScript a plus.
+* Experience shipping code to production - experience writing extensible and testable software, contributing to and maintaining a production codebase, and analyzing performance tradeoffs
+* Familiarity with software design patterns - experience designing scalable programs using common design patterns like factories, proxies, observers, etc. You’re comfortable with being handed a feature, designing the architecture for that feature, and knowing which design patterns to use to build an extensible product.
+* Independence in uncertainty - you’re not expected to know everything coming in, but you should be able to become an expert in what you’re working on and share your knowledge with the rest of the team.
 
-* JavaScript - great coding skills and an appetite to learn more (experience with ES6) 
-* Software design - some understanding of design patterns and ability to transform ideas into a stable working software that is testable and maintainable
-* CSS preprocessors like SASS/LESS/Stylus
-* SQL/noSQL databases
+We’re looking more for how you work than what you’ve worked on, but we’ll value any extra experience with these tools we’re already using: 
 
-##### Nice to Have
-
-* Client-side frameworks -  React, Angular or Vue
-* Nodejs and server-side frameworks (Express/Hapi)
-* Architecture - microservices design, messaging (kafka/rabbitMQ/kinesis)
+* Client-side frameworks - React, Angular or Vue
+* Node.js and server-side frameworks (Express/hapi)
 * API design (REST, GraphQL)
-* Experience with building responsive interfaces for mobile, desktop and everything in between (literally)
-* JavaScript ecosystem - packaging (yarn/npm), building (webpack), testing (mocha/jest/jasmine, headless browsers etc.), linting (jshint/eslint/jslint)
+* Architecture - microservices design, messaging (kafka/rabbitMQ/kinesis)
+* Experience with building responsive interfaces for mobile, desktop and every screen dimension in between
+* JavaScript ecosystem - packaging (yarn/npm), building (webpack), testing (mocha/jest/jasmine, headless browsers etc.)
 * Experience and familiarity with the browser rendering process, Canvas, WebGL, shaders
 * Infrastructure as code (Cloudformation / Terraform), monitoring
             `
         },
         {
             title: 'Senior Fullstack Developer',
+            introText: `
+eko is a media technology company pioneering a new medium in which viewers shape live action video stories as they unfold. Our award-winning shows are distributed through [eko.com](https://eko.com), affiliate partners, and social networks; and are viewable on desktop, mobile, connected TVs, and VR.
+
+We’re looking for a developer passionate about new technology and willing to push the envelope of what can be done on the web. Our platform is mainly web-based, so you’ll be working heavily with JavaScript, as well as other web libraries and tools (React, Redux, Angular, Node.js, and hapi to name a few). Our ultimate goal, though, is to always be at the forefront of video entertainment technology, so we’re not tied to any one tool and we’re always experimenting.
+
+At eko, you’ll: 
+* Contribute to every part of our stack- from working with SQL databases to building microservices to crafting complex UI
+* Have full ownership and responsibility over your feature work
+* Collaborate with almost every other department in the company
+* Become an expert in JavaScript rendering, I/O handling, and data processing
+* Navigate the challenges of supporting multiple environments and devices like computers, mobile phones, tablets, smart TVs, and gaming consoles
+* Help design and define what choice-driven entertainment really is, and what makes it tick
+
+*(and the dogs, snacks, happy hour jam sessions, and general speak-your-mind creative environment will keep things interesting when you’re not JavaScripting)*
+
+If you are all about creating mind-blowing experiences on the web, consider yourself invited!
+
+Want to learn more?
+            `,
             text: `
 ##### Key Requirements
 
-* JavaScript - great coding skills and an appetite to learn more
-* Software design - deep understanding of design patterns and ability to transform ideas into a stable working software that is testable and maintainable
-* CSS preprocessors like SASS/LESS/Stylus, CSS architecture methodologies like BEM/OOCSS/SMACSS
-* Experience with building responsive interfaces for mobile, desktop and everything in between (literally)
+* Programming skills - deep understanding of at least one programming language. Able to effectively communicate strengths/weaknesses in code. Experience with JavaScript a plus. 
+* Experience shipping code to production - Experience writing extensible and testable software, contributing to a production codebase. You’ve had a hand in every part of the stack- from building responsive interfaces for every screen dimension (using React/Angular/Vue) to working with NodeJS and server-side frameworks (Express/Hapi). 
+* Familiarity with software design patterns - comfortable with being handed a feature, designing the architecture for that feature, and knowing which design patterns to use to build an extensible product.
+* Fluency in architecture and API design -  experience developing REST/GraphQL APIs. Comfortable designing microservices with kafka/rabbitMQ/kinesis.
+* Independence in uncertainty - you’re not expected to know everything coming in, but you should be able to become an expert in what you’re working on and share your knowledge with the rest of the team.
+
+We’re looking more for how you work than what you’ve worked on, but we’ll value any extra experience with these tools we’re already using:
+ 
 * Infrastructure as code (Cloudformation / Terraform), monitoring
-* Nodejs and server-side frameworks (Express/Hapi)
-* Architecture - microservices design, messaging (kafka/rabbitMQ/kinesis)
-* API design (REST, GraphQL)
-* SQL/noSQL databases
-
-
-##### Nice to Have
-
-* Client-side frameworks -  React, Angular or Vue
-* CSS - preprocessors like SASS/LESS/Stylus, CSS architecture methodologies like BEM/OOCSS/SMACSS
-* JavaScript ecosystem - packaging (yarn/npm), building (webpack), testing (mocha/jest/jasmine, headless browsers etc.), linting (jshint/eslint/jslint)
+* JavaScript ecosystem - packaging (yarn/npm), building (webpack), testing (mocha/jest/jasmine, headless browsers etc.)
 * Experience and familiarity with the browser rendering process, Canvas, WebGL, shaders
             `
         },
