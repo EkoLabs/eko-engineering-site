c379492867f735347ef7144008786913edf6a95e
Yariv Fruend <yariv@helloeko.com>
2020-07-28 15:26:04 +0300

    Updated the gallery

--- src/components/office/Office.jsx
+++ src/components/office/Office.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Gallery from "../gallery/Gallery";
 
-import drums from "../../media/office_photos/drums.jpg";
-import eko from "../../media/office_photos/eko.jpg";
-import juna from "../../media/office_photos/juna.jpg";
-import kitchen from "../../media/office_photos/kitchen.jpg";
-import mario  from "../../media/office_photos/mario.jpg";
-import piano from "../../media/office_photos/piano.jpg";
-import pingpong from "../../media/office_photos/pingpong.jpg";
-import posters from "../../media/office_photos/posters.jpg";
+import buddies from "../../media/office_photos/dog-buddies.jpg";
+import happyCoding0 from "../../media/office_photos/happy-coding-0.jpg";
+import happyCoding1 from "../../media/office_photos/happy-coding-1.jpg";
+import happyHour from "../../media/office_photos/happy-hour.jpg";
+import hofshy from "../../media/office_photos/hofshy.jpg";
+import lunch from "../../media/office_photos/lunch.jpg";
+import maayan from "../../media/office_photos/maayan.jpg";
+import ran from "../../media/office_photos/ran.jpg";
 import ruel from "../../media/office_photos/ruel.jpg";
-import shows from "../../media/office_photos/shows.jpg";
+import twins from "../../media/office_photos/twins.jpg";
 
-let images = [eko, ruel, mario, posters, drums, kitchen, shows, piano, juna,  pingpong];
+let images = [happyCoding0, happyHour, maayan, happyCoding1, twins, buddies, ran, hofshy, lunch, ruel];
 
-function Office(){
+function Office() {
     let data = {
         title: 'Behind the scenes',
         items: images.map(imageURL => ({
