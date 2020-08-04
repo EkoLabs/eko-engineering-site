4054317a01ff7d1cce54056bb8e6d70d969e7c03
OpherV <opherv@gmail.com>
2020-08-04 16:11:55 +0300

    add JS to Sonorous name #3

--- src/components/Sonorous/Sonorous.jsx
+++ src/components/Sonorous/Sonorous.jsx
@@ -69,10 +69,10 @@ function Sonorous(props){
             <section className="sonorous">
                 <div className="content">
                     <header>
-                        <img src={logoImage} alt="Sonorous Logo" title="Sonorous"/>
+                        <img src={logoImage} alt="Sonorous.js Logo" title="Sonorous"/>
                         <div className="text">
                             <h1>Sonorous<span> . js</span></h1>
-                            <h2>Sonorous is a JavaScript audio library built for the modern browser. It enables easy audio integration into web apps and games using WebAudio APIs, with fine-grained control for those who need it, while handling any cross-browser issues for you.</h2>
+                            <h2>Sonorous.js is a JavaScript audio library built for the modern browser. It enables easy audio integration into web apps and games using WebAudio APIs, with fine-grained control for those who need it, while handling any cross-browser issues for you.</h2>
                         </div>
                     </header>
                     <iframe scrolling="no" title="Sonorous Example" className="codepen"
