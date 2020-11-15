3a8b3980c703317e69d7c1c2211ba99ea876fc44
Opher Vishnia <opherv@gmail.com>
2020-11-15 11:28:24 +0200

    Update LandingPageQa.jsx

--- src/pages/landingPageQa/LandingPageQa.jsx
+++ src/pages/landingPageQa/LandingPageQa.jsx
@@ -33,7 +33,7 @@ export default function LandingPageQa(props){
                         <li>Provide critical insights and input throughout the product dev process</li>
                         <li>Explore real-world CI/CD automation scripts and scenarios</li>
                         <li>Shape the future of eCommerce and digital entertainment</li>
-                        <li>Deliver features to millions of users/viewers</li>
+                        <li>Deliver features to millions of viewers</li>
                         <li>Oh, did we mention that you’ll be joining <a href="https://www.duns100.co.il/rating/%D7%94%D7%99%D7%99%D7%98%D7%A7/%D7%94%D7%A1%D7%98%D7%90%D7%A8%D7%98%D7%90%D7%A4%D7%99%D7%9D_%D7%A9%D7%94%D7%9B%D7%99_%D7%98%D7%95%D7%91_%D7%9C%D7%A2%D7%91%D7%95%D7%93_%D7%91%D7%94%D7%9D#eko" target="_blank">one of Israel’s top 20 startups to work for in 2020 by Dun & Bradstreet?</a></li>
                     </ul>
                     <div class="wrapup">We’re looking for good people, so if any or all speak to you, let’s talk.</div>
