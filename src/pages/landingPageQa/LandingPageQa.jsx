11bb518106a68902bf02fbdc141c2ff2cd1b8470
OpherV <opherv@gmail.com>
2020-11-17 17:43:25 +0200

    Update QA landing page text

--- src/pages/landingPageQa/LandingPageQa.jsx
+++ src/pages/landingPageQa/LandingPageQa.jsx
@@ -18,7 +18,7 @@ export default function LandingPageQa(props){
                         </div>
                     </div>
                     <div className="right">
-                        <h3>If you want to reinvent the QA process in a one-of-a-kind environment, <em>we should talk.</em></h3>
+                        <h3>If you’re a natural-born QA engineer who loves to understand how systems work, <em>let's talk.</em></h3>
                     </div>
                 </div>
             </section>
@@ -30,10 +30,10 @@ export default function LandingPageQa(props){
                         <li>To us, QA engineers are first-class citizens</li>
                         <li>You’ll lead projects from start to finish</li>
                         <li>Be an integral part of a cross-functional team</li>
+                        <li>Go beyond the spec and imagine the entire experience from the users' perspective</li>
                         <li>Provide critical insights and input throughout the product dev process</li>
-                        <li>Explore real-world CI/CD automation scripts and scenarios</li>
-                        <li>Shape the future of eCommerce and digital entertainment</li>
-                        <li>Deliver features to millions of viewers</li>
+                        <li>Design, develop and execute test scripts</li>
+                        <li>Lead manual testing of new features in a complex environment</li>
                         <li>Oh, did we mention that you’ll be joining <a href="https://www.duns100.co.il/rating/%D7%94%D7%99%D7%99%D7%98%D7%A7/%D7%94%D7%A1%D7%98%D7%90%D7%A8%D7%98%D7%90%D7%A4%D7%99%D7%9D_%D7%A9%D7%94%D7%9B%D7%99_%D7%98%D7%95%D7%91_%D7%9C%D7%A2%D7%91%D7%95%D7%93_%D7%91%D7%94%D7%9D#eko" target="_blank">one of Israel’s top 20 startups to work for in 2020 by Dun & Bradstreet?</a></li>
                     </ul>
                     <div class="wrapup">We’re looking for good people, so if any or all speak to you, let’s talk.</div>
