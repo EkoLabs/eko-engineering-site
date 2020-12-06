5f635a89bebb8605181385a2d8848deceeb65b0c
Yariv Fruend <yariv@eko.com>
2020-12-06 16:05:16 +0200

    oops

--- src/components/contactUsForm/ContactUsForm.jsx
+++ src/components/contactUsForm/ContactUsForm.jsx
@@ -50,7 +50,7 @@ function ContactForm(props) {
     let error = formState === "error";
     let formDisabled = isSending || success;
     let positionForm = props.formType === 'position';
-    let formAction = `https://hooks.slack.com/services/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`;
+    let formAction = `https://hooks.slack.com/services/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`;
     let title = 'Contact Us';
     
     return (
