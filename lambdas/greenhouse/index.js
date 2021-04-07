99134518681773fe3dda528804732006673b2469
OpherV <opherv@gmail.com>
2021-04-07 17:41:47 +0300

    More fun with lambdas!

--- lambdas/greenhouse/index.js
+++ lambdas/greenhouse/index.js
@@ -1,57 +1,112 @@
-function getFormData(event) {
-    const result = {
-        name: 'oops',
-        position: '',
-        notes: '',
-        resumeUrl: null,
-        resumeFilename: null,
-        site: null,
-        followers: []
-    }
+const axios = require('axios')
+const GREENHOUSE_KEY = process.env.GREENHOUSE_KEY;
 
-    // parse form
-    let formData = {};
-    try {
-        formData = JSON.parse(event.body);
-    }
-    catch (e) {
-        formData = {
-            submission: {
-                _date: ''
-            },
-            form: 'oops',
-        };
-    }
+const Busboy = require('busboy');
 
-    if (formData.form) {
-        // eko engineering form
-        const submission = formData.submission;
+const parser = (event) => new Promise((resolve, reject) => {
+    const busboy = new Busboy({
+        headers: event.headers
+    });
 
-        result.positionId = submission.positionId;
-        result.position = submission.positionTitle;
-        result.site = submission.site;
-        result.notes = `${submission.message}\n\nHow did you hear about us?: ${submission.howDidYouHearAboutUs}\nPosition: ${submission.positionTitle}\nEmail: ${submission.email}\nOffice: ${submission.site}\n${submission.utm_campaign ? `utm_campaign: ${submission.utm_campaign}\n` : ``}${submission.utm_source ? `utm_source: ${submission.utm_source}\n` : ``}${submission.utm_medium ? `utm_medium: ${submission.utm_medium}\n` : ``}${submission.utm_term ? `utm_term: ${submission.utm_term}\n` : ``}${submission.utm_content ? `utm_content: ${submission.utm_content}\n` : ``}\nTimestamp: ${submission._date}`;
-        result.name = submission.name;
-        result.resumeUrl = submission.cv || submission.attachment;
-        if (result.resumeUrl) {
-            let cvArray = result.resumeUrl.split('/');
-            result.resumeFilename = cvArray[cvArray.length -1];
+    let result = {
+        files: []
+    };
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        file.on('data', data => {
+            result.files.push({
+                file: data,
+                fileName: filename,
+                contentType: mimetype
+            });
+        });
+    });
+
+    busboy.on('field', (fieldname, value) => {
+        try {
+            result[fieldname] = JSON.parse(value);
+        } catch (err) {
+            result[fieldname] = value;
         }
-    }
-    else {
-        result.notes = `Oops, form cannot be handled properly. ${formData.form} @ ${formData.submission._date}`;
-    }
+    });
 
-    return result;
+    busboy.on('error', error => reject(`Parse error: ${error}`));
+    busboy.on('finish', () => {
+        event.body = result;
+        resolve(event);
+    });
 
+    busboy.write(event.body, event.isBase64Encoded ? 'base64' : 'binary');
+    busboy.end();
+});
+
+
+function createResponse(message) {
+    return {
+        'statusCode': 200,
+        // 'headers': { 'Content-Type': 'application/json' },
+        headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+        },
+        'body': JSON.stringify({ 'msg': message || 'ok' })
+    }
 }
 
-exports.handler = async (event) => {
-    let formData = getFormData(event);
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('new lambda! 555'),
-    };
-    return response;
+exports.handler = async (event, context) => {
+    try {
+        await parser(event);
+    } catch (e){
+        console.error('parse error!', e);
+        console.error('Dumping event:', JSON.stringify(event));
+    }
+    let formData = event.body;
+
+    // console.log('formdata',JSON.stringify(formData));
+
+    let jobData ={
+        boardToken: "ekoisrael",
+        jobId: 5016885002
+    }
+
+    // form has only one field for text, and greenhouse requires first and last names, so we split a first space
+    let [firstName, ...moreNames] = formData.name.split(" ")
+
+    let resumeContent = formData.files[0].file.toString('base64');
+
+    let candidateData = {
+        first_name: firstName,
+        last_name: moreNames.join(" "),
+        email: formData.email,
+        phone: formData.phone,
+        resume_content: resumeContent,
+        resume_content_filename: formData.files[0].fileName,
+        custom_fields: {
+            "how_did_you_hear_about_us": formData.howDidYouHearAboutUs,
+            "_something_interesting_about_yourself_": formData.message
+        },
+    }
+
+    let jobPostTarget = `https://boards-api.greenhouse.io/v1/boards/${jobData.boardToken}/jobs/${jobData.jobId}`
+    let encodedKey = Buffer.from(GREENHOUSE_KEY).toString('base64');
+    let authHeader = `Basic ${encodedKey}`;
+
+    await axios
+        .post(jobPostTarget, {
+            ...candidateData
+            },{ headers: {
+                Authorization: authHeader,
+                "Content-Type": "application/json"
+            }})
+        .then(res => {
+            console.log(`statusCode: ${res.statusCode}`)
+            // console.log('res', res)
+        })
+        .catch(error => {
+            console.error('Greenhouse post error!', error);
+            console.error('Dumping form data', JSON.stringify(event.body));
+        })
+
+    return createResponse();
 };
--- scripts/update_lambda.js
+++ scripts/update_lambda.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const ChildProcess = require('child_process')
+const hashFiles = require('hash-files');
 
 const yargs = require('yargs');
 const archiver = require('archiver');
@@ -9,66 +10,121 @@ AWS.config.region = process.env.AWS_REGION; // set your Environment Variables...
 
 const lambda = new AWS.Lambda();
 
-let skipHashCheck = yargs.argv.skipHashCheck;
 let noUpload = yargs.argv.noUpload;
+
+const GLOB = '**/*.!(zip)';
 
 function updateLambdaCode(lambdaDirName){
     let package = require(`../lambdas/${lambdaDirName}/package.json`);
     let functionName = package.name;
 
-    let latestCommitHash = ChildProcess.execSync('git rev-parse --short HEAD').toString().trim();
-
-    lambda.getFunctionConfiguration({ FunctionName: functionName }).promise()
-        .then(originalLambda => {
-            if (skipHashCheck || originalLambda.Description !== latestCommitHash){
-                console.log(`Old lambda code hash is ${originalLambda.Description}, new is ${latestCommitHash}. Updating lambda code`);
-                createZipAndUploadLambda(lambdaDirName, functionName, latestCommitHash)
-            } else {
-                console.log('Lambda code is identical. Skipping update');
-            }
-        })
-        .catch(err => { throw  new Error(err) });
-}
-
-function createZipAndUploadLambda(lambdaDirName, functionName, hash){
     let basePath = process.cwd();
     let lambdaDirPath = path.join(basePath, "lambdas/",lambdaDirName);
+
+
+    runNpmInstall(lambdaDirPath)
+    .then(
+        () => Promise.all([
+            createZip(lambdaDirPath),
+            hashContent(lambdaDirPath),
+            lambda.getFunctionConfiguration({ FunctionName: functionName }).promise()
+        ])
+    )
+    .then(promiseValues => {
+        [zipFilePath, contentHash, lambdaValue] = promiseValues;
+
+        if (lambdaValue.Description !== contentHash){
+            console.log(`Old lambda code hash is ${lambdaValue.Description}, new is ${contentHash}. Updating lambda code`);
+            uploadLambda(functionName, zipFilePath, contentHash)
+        } else {
+            console.log(lambdaValue.Description, contentHash);
+            console.log('Lambda code is identical. Skipping update');
+        }
+    })
+    .catch(err => { throw  new Error(err) });
+
+}
+
+function runNpmInstall(lambdaDirPath){
+    console.log("Running npm install in lambda dir");
+    return new Promise((resolve, reject) => {
+        ChildProcess.exec('npm install', {cwd: lambdaDirPath}, (err, stout, sterr) => {
+            if (err) {
+                reject({err, sterr});
+            } else {
+                console.log("Done running npm install");
+                resolve(stout);
+            }
+        });
+    });
+}
+
+function hashContent(lambdaDirPath){
+    let hashGlob = path.join(lambdaDirPath, GLOB);
+    return new Promise((resolve,reject) => {
+        console.log(`hashContent: Hashing dir content... ${hashGlob}`);
+        hashFiles({files: hashGlob}, function(error, hash) {
+            if (error){
+                reject(error);
+            } else {
+                console.log('hashContent: Done hashing:', hash);
+                resolve(hash)
+            }
+        });
+    })
+
+}
+
+function createZip(lambdaDirPath){
+    let lambdaDirName = path.basename(lambdaDirPath);
     let zipFilePath = path.join(lambdaDirPath, `/${lambdaDirName}.zip`);
+    console.log(`createZip: Writing zip file: ${zipFilePath}`);
 
     // create zip
     const archive = archiver('zip', {
         zlib: { level: 9 } // Sets the compression level.
     });
     const output = fs.createWriteStream(zipFilePath);
-    archive.glob('**.!(zip)', {cwd: lambdaDirPath}); // add all files in the dir, ignore zip files
+    archive.glob(GLOB, {cwd: lambdaDirPath}); // add all files in the dir, ignore zip files
     archive.pipe(output);
 
+    let promise = new Promise((resolve, reject) => {
+        // good practice to catch this error explicitly
+        archive.on('error', function(err) {
+            reject(err)
+        });
 
-    output.on('close', function() {
-        console.log(`Zip file written. ${archive.pointer()} total bytes`);
 
-        let  UPDATE_CONFIG = {
-            FunctionName: functionName,
-            Description: hash
-        };
-
-        let UPDATE_PARAMS = {
-            FunctionName: functionName,
-            ZipFile: fs.readFileSync(zipFilePath)
-        };
-
-        if (noUpload){
-            console.log("Dry run - skipping actual upload")
-        } else {
-            lambda.updateFunctionConfiguration(UPDATE_CONFIG).promise()
-                .then( response => lambda.updateFunctionCode(UPDATE_PARAMS).promise())
-                .then( response => console.log(`lambda code for ${lambdaDirName} updated!`))
-                .catch( err => { throw  new Error(err) });
-        }
-    });
+        output.on('close', function() {
+            console.log(`createZip: Zip file written. ${archive.pointer()} total bytes`);
+            resolve(zipFilePath)
+        });
+    })
 
     archive.finalize();
- }
+
+    return promise;
+}
+
+function uploadLambda(functionName, zipFilePath, description){
+    let UPDATE_CONFIG = {
+        FunctionName: functionName,
+        Description: description
+    };
+    let UPDATE_PARAMS = {
+        FunctionName: functionName,
+        ZipFile: fs.readFileSync(zipFilePath)
+    };
+
+    if (noUpload){
+        console.log("Dry run - skipping actual upload")
+    } else {
+        lambda.updateFunctionConfiguration(UPDATE_CONFIG).promise()
+            .then( response => lambda.updateFunctionCode(UPDATE_PARAMS).promise())
+            .then( response => console.log(`lambda code for ${functionName} updated!`))
+            .catch( err => { throw  new Error(err) });
+    }
+}
 
 if (yargs.argv.lambda){
     updateLambdaCode(yargs.argv.lambda);
