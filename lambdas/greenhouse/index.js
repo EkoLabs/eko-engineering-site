c15edf1b9f0d29bcabad952c5ec445a0968c4910
OpherV <opherv@gmail.com>
2023-02-19 13:48:39 +0200

    Edit greenhouse lambda

--- lambdas/greenhouse/index.js
+++ lambdas/greenhouse/index.js
@@ -145,7 +145,7 @@ exports.handler = async (event, context) => {
 
     const greenhouseKey = await archer.get('eko-engineering-site').greenhouse_key;
     let jobPostTarget = `https://boards-api.greenhouse.io/v1/boards/${jobData.boardToken}/jobs/${jobData.jobId}`
-    let encodedKey = Buffer.from(greenhouseKey).toString('base64');
+    let encodedKey = greenhouseKey.toString('base64');
     let authHeader = `Basic ${encodedKey}`;
 
     console.log('Posting candidate data to greenhouse, url ', jobPostTarget);
